6dbca8e940b0d2e6546b516d3804918b9fd006a9
Iago <iago.gc88@gmail.com>
2021-02-18 23:05:33 +0100

    Proveedores y piezas

---
+++ NET_API_SQL_Proveedores/Models/Pieza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_API_SQL_Proveedores.Models {
+    public partial class Pieza {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public ICollection<Suministro> Suministros { get; set; }
+        public Pieza() {
+            Suministros = new HashSet<Suministro>();
+        }
+    }
+}
---
+++ NET_API_SQL_Proveedores/Models/Suministro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_API_SQL_Proveedores.Models {
+    public partial class Suministro {
+        public int CodigoPieza { get; set; }
+        public char ProveedorId { get; set; }
+        public int Precio { get; set; }
+        public virtual Proveedor Proveedor { get; set; }
+        public virtual Pieza Pieza { get; set; }
+    }
+}
